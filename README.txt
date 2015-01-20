163f2a31b7cb9668cc9f33c75764540ccdc91226
Chanceko <chanceoday@gmail.com>
2015-01-19 22:17:45 -0800

    Initial Commit
    Version 1.0 (Homework 1)

---
+++ README.txt
@@ -0,0 +1,21 @@
+-----------------------------------README--------------------------------------
+To run this Go program, Go must be installed on your system and the Root
+and Path must be appropriately configured.
+-------------------------------------------------------------------------------
+1.  Open your Console or Console program (e.g. iTerm on MacOSX)
+2.  Navigate to the appropriate directory where this Go program is located
+    from the Console (e.g. cd Documents/Go Docs)
+3.  Input the following statement into the Console command line:
+		go run timeserver.go
+3a. If desired, the following flags can be appended to the previous command
+		--port (portvalue)
+		-V
+4.  Open your preferred web browser, and navigate to:
+		http://localhost:(portvalue)/time
+		NOTE: The default portvalue is 8080.
+
+To terminate the program, use the following keystroke:  (control + c)
+
+-------------------------------------------------------------------------------
+						Copyright 2015 - Chance O'Day
+-------------------------------------------------------------------------------
---
+++ timeserver.go
@@ -0,0 +1,83 @@
+// Chance O'Day
+// CSS 490 - Tactical SWE
+// 1/13/15
+// Assignment 1
+
+// This package acts as a primitive web server hosted on the user's local
+// machine.  It displays the current time when "/time" is appended to the
+// localhost address.  It's default port is 8080, but allows the server
+// to be hosted on a non-default port when the flag "-port" is included.
+// This package also includes a "-V" flag, which outputs to the console
+// the current package version.  If any other address is used except 
+// "/time", an error message is displayed in the browser.  If the
+// target port is already in use, the package will output an error
+// message to the console.  When time is displayed, it is formatted
+// as Hour:Minute:Second (AM/PM).
+package main
+
+import (
+	"fmt"
+	"time"
+	"flag"
+	"net/http"
+)
+
+// General handler.  Only used when "/time" is appended to the web address
+func handler(response http.ResponseWriter, request *http.Request) {
+    s := currentTime()
+    fmt.Fprintf(response, `
+    	<html>
+		<head>
+		<style>
+		p {font-size: xx-large}
+		span.time {color: red}
+		</style>
+		</head>
+		<body>
+		<p>The time is now <span class="time">%v</span>.</p>
+		</body>
+		</html>
+    	`, s)
+
+}
+
+// Handler for all cases where the web address provided is not "/time"
+func errorHandler(response http.ResponseWriter, request *http.Request) {
+	response.WriteHeader(404)
+	fmt.Fprint(response, `
+		<html>
+		<body>
+		<p>These are not the URLs you're looking for.</p>
+		</body>
+		</html>
+		`)
+}
+
+// Parses flags and starts the server.  Prints an error message
+// if the target port is already in use.
+func main() {
+	port := flag.Int("port", 8080, "Sets the server port")
+	version := flag.Bool("V", false, "Program version number")
+	flag.Parse()
+	
+	portString := fmt.Sprintf(":%v", *port)
+
+	if *version {
+		fmt.Println("Current Program Version: 1.0")
+		return
+	}
+
+	http.HandleFunc("/", errorHandler)
+    http.HandleFunc("/time", handler)
+    error := http.ListenAndServe(portString , nil)
+
+    fmt.Printf("Server already in use.  Error %v", error)
+}
+
+// Returns the current time in the format Hour:Minute:Second (AM/PM)
+func currentTime() string {
+	const layout = "03:04:05 PM"
+	t := time.Now()
+	s := t.Format(layout)
+	return s;
+}
