8e576ba5599db07f2674d349a5491ba972ea5258
Chanceko <chanceoday@gmail.com>
2015-01-19 22:19:53 -0800

    Cookies Implemented
    Incomplete

--- timeserver.go
+++ timeserver.go
@@ -1,7 +1,7 @@
 // Chance O'Day
 // CSS 490 - Tactical SWE
-// 1/13/15
-// Assignment 1
+// 1/19/2015
+// Assignment 2
 
 // This package acts as a primitive web server hosted on the user's local
 // machine.  It displays the current time when "/time" is appended to the
@@ -13,6 +13,8 @@
 // target port is already in use, the package will output an error
 // message to the console.  When time is displayed, it is formatted
 // as Hour:Minute:Second (AM/PM).
+
+//CITE:: http://stackoverflow.com/questions/12756782/go-http-post-and-use-cookies
 package main
 
 import (
@@ -20,10 +22,35 @@ import (
 	"time"
 	"flag"
 	"net/http"
+	"os/exec"
 )
 
+// Parses flags and starts the server.  Prints an error message
+// if the target port is already in use.
+func main() {
+	port := flag.Int("port", 8080, "Sets the server port")
+	version := flag.Bool("V", false, "Program version number")
+	flag.Parse()
+
+	cookieJar := make(map[string]string)
+	
+	portString := fmt.Sprintf(":%v", *port)
+
+	if *version {
+		fmt.Println("Current Program Version: 1.0")
+		return
+	}
+
+	http.HandleFunc("/", homePageHandler)
+	http.HandleFunc("/login?name=name", formHandler)
+    http.HandleFunc("/time", timeHandler)
+    error := http.ListenAndServe(portString , nil)
+
+    fmt.Printf("Server already in use.  Error %v", error)
+}
+
 // General handler.  Only used when "/time" is appended to the web address
-func handler(response http.ResponseWriter, request *http.Request) {
+func timeHandler(response http.ResponseWriter, request *http.Request) {
     s := currentTime()
     fmt.Fprintf(response, `
     	<html>
@@ -38,40 +65,44 @@ func handler(response http.ResponseWriter, request *http.Request) {
 		</body>
 		</html>
     	`, s)
-
 }
 
 // Handler for all cases where the web address provided is not "/time"
-func errorHandler(response http.ResponseWriter, request *http.Request) {
-	response.WriteHeader(404)
+func formHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(response, `
-		<html>
-		<body>
-		<p>These are not the URLs you're looking for.</p>
-		</body>
-		</html>
+	<html>
+	<body>
+	<form action="login">
+	  What is your name, Earthling?
+	  <input type="text" name="name" size="50">
+	  <input type="submit">
+	</form>
+	</p>
+	</body>
+	</html>
 		`)
+	request.ParseForm()
+	formName := request.FormValue("name")
+	cookieUUID, _ := exec.Command("uuidgen").Output()
+	uuidString := fmt.Sprintf("%v", cookieUUID)
+
+	cookie := &http.Cookie{Name:"COOKIE", Value:uuidString, Expires:time.Now().Add(356*24*time.Hour), HttpOnly:true}
+	http.SetCookie(response, cookie)
+
+	cookieJar[uuidString] = formName
+
+	http.Redirect(response, request, "/", http.StatusFound)
+
 }
 
-// Parses flags and starts the server.  Prints an error message
-// if the target port is already in use.
-func main() {
-	port := flag.Int("port", 8080, "Sets the server port")
-	version := flag.Bool("V", false, "Program version number")
-	flag.Parse()
-	
-	portString := fmt.Sprintf(":%v", *port)
+func homePageHandler(response http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("COOKIE")
 
-	if *version {
-		fmt.Println("Current Program Version: 1.0")
-		return
+	if len(cookieJar) > 0 && cookieJar[cookie.Value] != "" {
+		fmt.Fprintf(response, "Greetings, %v", cookieJar[cookie.Value])
+	} else {
+		http.Redirect(response, request, "/login?name=name", http.StatusFound)
 	}
-
-	http.HandleFunc("/", errorHandler)
-    http.HandleFunc("/time", handler)
-    error := http.ListenAndServe(portString , nil)
-
-    fmt.Printf("Server already in use.  Error %v", error)
 }
 
 // Returns the current time in the format Hour:Minute:Second (AM/PM)
